5bfca1e6be98c57c5de8e2be8f75d769c12b8d3d
Bilgen Kaan Remzi <137759282+Rekabil@users.noreply.github.com>
2023-08-22 14:52:51 +0200

    first

---
+++ script.js
@@ -0,0 +1,23 @@
+window.onload = function () {
+  document.getElementById("yourName").innerHTML = localStorage.getItem("user");
+};
+let tempo = 0;
+
+const timer = () => {
+  setInterval(function () {
+    tempo++;
+    sessionStorage.setItem("time", tempo);
+    document.getElementById("timer").innerHTML = Math.fixed(tempo / 1000);
+  });
+};
+function inputName(event) {
+  const name = document.getElementById("name").value;
+  localStorage.setItem("user", name);
+  document.getElementById("yourName").innerHTML = name;
+}
+
+function removeName(event) {
+  localStorage.removeItem("user");
+  document.getElementById("yourName").innerHTML = "";
+}
+timer();
